89b793fa7c8e00f36578870c5547d22a009e4323
thegig009 <loveesesgig2005@gmail.com>
2025-12-16 19:43:12 +0700

    Add files via upload

---
+++ winForm_THEGIG/Form3.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace winForm_THEGIG
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(487, 147);
+            button1.Name = "button1";
+            button1.Size = new Size(151, 46);
+            button1.TabIndex = 0;
+            button1.Text = "คำนวน";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(212, 147);
+            label1.Name = "label1";
+            label1.Size = new Size(284, 431);
+            label1.TabIndex = 1;
+            label1.Text = "เเสดงผล";
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1035, 615);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form3";
+            Text = "Jagged Array";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+    }
+}
---
+++ winForm_THEGIG/Form3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace winForm_THEGIG
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int[][] MyArr = new int[][]
+	{
+		new int[] {1, 3, 5, 7, 9},
+		new int[] {0, 2, 4, 6},
+		new int[] {11, 22}
+	};
+
+			label1.Text = "";   // เคลียร์ข้อความเดิม
+
+			for (int i = 0; i < MyArr.Length; i++)
+			{
+				int sum = 0;
+
+				for (int j = 0; j < MyArr[i].Length; j++)
+				{
+					label1.Text += MyArr[i][j] + " ";
+					sum += MyArr[i][j];
+				}
+
+				label1.Text += Environment.NewLine;
+				label1.Text += "Sum = " + sum + Environment.NewLine;
+				label1.Text += "---end---" + Environment.NewLine;
+			}
+		}
+    }
+}
